14d035583268e35d379c3eb9bab5c2b6199d0ee6
sayboud <boudsalakham.saynorlath@edu.em-lyon.com>
2025-03-28 18:23:03 +0100

    Update PeakTimeVis.js

--- Visualization Page/PeakTimeVis.js
+++ Visualization Page/PeakTimeVis.js
@@ -2,7 +2,7 @@ const margin = { top: 50, right: 30, bottom: 40, left: 80 };
 const width = 1200 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
 
-d3.csv("charging-session-cleaned.csv").then(function(df) {
+d3.csv("charging-sessions-cleaned.csv").then(function(df) {
 
 console.log("loaded")
 
