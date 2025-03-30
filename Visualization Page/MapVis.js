e2260dd0e5623d0f661dfe205a09144ee62347fa
sayboud <boudsalakham.saynorlath@edu.em-lyon.com>
2025-03-30 22:18:25 +0200

    map

--- Visualization Page/MapVis.js
+++ Visualization Page/MapVis.js
@@ -1,15 +1,10 @@
-
-const width = 1200
-const height = 1200
-
+const map = L.map('interactive-map').setView([37.4419, -122.1430], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+console.log('done')
 
 let df = [];
-
-
-const svg = d3.select('#interactive-map').append('svg').attr('width',width)
-.attr('height',height);
-
-
 
 
 
